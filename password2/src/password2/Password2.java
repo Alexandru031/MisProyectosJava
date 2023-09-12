984319ea5ff03ba1621879e730554c72feb7ad10
Alexandru031 <alexandrustanica33@gmail.com>
2023-09-12 10:24:43 +0200

    añade password

---
+++ password2/src/password2/Password2.java
@@ -0,0 +1,26 @@
+/*
+Escriu un programa que llig de teclat un password, si és vàlid, es visualitza “el password és 
+vàlid” i s’acaba el programa. Si el password és invàlid, es visualitza “el password és 
+invàlid\nha de tindre almenys 8 caràcters, lletres minúscules i majúscules i conté dos 
+dígits com a mínim” i es repeteix la lectura del password. Per a ser vàlid, un password ha 
+de complir: té almenys 8 caràcters, conté lletres minúscules i majúscules i conté dos dígits 
+com a mínim. Usa mètodes de String i de Character.
+ */
+package password2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class Password2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
